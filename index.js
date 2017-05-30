cbedb6caf6c312aea52c33c277c4a9ddd487984d
Ahmed Eid <a.eid@yandex.com>
2017-05-31 01:07:23 +0300

    add url as a query string in the url

--- index.js
+++ index.js
@@ -6,15 +6,17 @@ const cheerio = require('cheerio')
 const app = express()
 
 app.get('/scrape', (req, res) => {
-  const url = 'http://www.imdb.com/title/tt1229340/';
+  let url = req.query.url
+  if (!req.query.url)
+    return res.end('provide a url')
 
   R(url, (err, response, html) => {
     if (err) return res.end(`Error: ${err}`)
     let $ = cheerio.load(html)
     res.json({
-      year: $('#titleYear a' , '.title_wrapper').text(),
-      title: $('h1','.title_wrapper').text(),
-      rating: $('[itemprop=ratingValue]' , '.ratingValue').text()
+      year: $('#titleYear a', '.title_wrapper').text(),
+      title: $('h1', '.title_wrapper').text(),
+      rating: $('[itemprop=ratingValue]', '.ratingValue').text()
     })
   })
 })
